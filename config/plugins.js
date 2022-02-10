eebef372b488bc80109273180ed47d5185d850a3
Jonácsik Ádám <68539743+adamjonacsik@users.noreply.github.com>
2022-02-10 23:59:20 +0100

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,9 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "cloudinary",
     providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+      localServer: {
+        maxage: 300000,
+      },
     },
   },
   email: {
